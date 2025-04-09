5981dfc7c08d2b335f53072f563537be093e657e
lythraa <lythraa@gmail.com>
2025-04-08 23:16:34 -0500

    modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,8 @@ module co.edu.uniquindio.poo.billeteravirtual {
 
 
     opens co.edu.uniquindio.poo.billeteravirtual to javafx.fxml;
-    exports co.edu.uniquindio.poo.billeteravirtual;
+    exports co.edu.uniquindio.poo.billeteravirtual.app;
+    opens co.edu.uniquindio.poo.billeteravirtual.app to javafx.fxml;
+    exports co.edu.uniquindio.poo.billeteravirtual.controllers;
+    opens co.edu.uniquindio.poo.billeteravirtual.controllers to javafx.fxml;
 }
