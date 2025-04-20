b38b826ed39027365fc2b898d3207b0d539b4413
lythraa <lythraa@gmail.com>
2025-04-19 20:45:46 -0500

    views and controller

---
+++ src/main/java/co/edu/uniquindio/poo/billeteravirtual/viewcontroller/AdministradorController.java
@@ -0,0 +1,58 @@
+package co.edu.uniquindio.poo.billeteravirtual.viewcontroller;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AdministradorController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label textoHolaUsuario;
+
+    @FXML
+    void onGestionarTransacciones() {
+
+    }
+
+    @FXML
+    void onGestionarUsuarios() {
+
+    }
+
+    @FXML
+    void onGestionarCuentasBancarias() {
+
+    }
+
+    @FXML
+    void onVerEstadisticas() {
+
+    }
+
+    @FXML
+    void onEditarPerfil() {
+
+    }
+
+    @FXML
+    void onCerrarSesion() {
+
+    }
+
+    @FXML
+    void onContactenos() {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert textoHolaUsuario != null : "fx:id=\"textoHolaUsuario\" was not injected: check your FXML file 'AdministradorView.fxml'.";
+
+    }
+}
