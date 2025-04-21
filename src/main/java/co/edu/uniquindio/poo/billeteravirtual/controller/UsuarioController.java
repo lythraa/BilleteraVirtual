a8240c9aa9b76a3459e2abae3a48803d56de7221
lythraa <lythraa@gmail.com>
2025-04-20 21:20:25 -0500

    views and controller

---
+++ src/main/java/co/edu/uniquindio/poo/billeteravirtual/controller/UsuarioController.java
@@ -0,0 +1,52 @@
+package co.edu.uniquindio.poo.billeteravirtual.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class UsuarioController {
+
+    @FXML
+    private Label textoSaldo;
+
+    @FXML
+    private Label textoHolaUsuario;
+
+    @FXML
+    void onTransacciones() {
+
+    }
+
+    @FXML
+    void onPresupuestos() {
+
+    }
+
+    @FXML
+    void onCuentas() {
+
+    }
+
+    @FXML
+    void onEditarPerfil() {
+
+    }
+
+    @FXML
+    void onCerrarSesion() {
+
+    }
+
+    @FXML
+    void enlaceSoporte() {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert textoSaldo != null : "fx:id=\"textoSaldo\" was not injected: check your FXML file 'UsuarioView.fxml'.";
+        assert textoHolaUsuario != null : "fx:id=\"textoHolaUsuario\" was not injected: check your FXML file 'UsuarioView.fxml'.";
+
+    }
+}
